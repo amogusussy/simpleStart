b72dfa3f1056a527771fecdf53a93719b8b78813
amogusussy <83502633+amogusussy@users.noreply.github.com>
2023-01-17 21:09:46 +0000

    Update main.js

--- main.js
+++ main.js
@@ -31,13 +31,13 @@ function twelveHour(a){
 
 function wordDay(int) {
     switch (int) {
+        case 0: return "SUN";
         case 1: return "MON";
         case 2: return "TUE";
         case 3: return "WED";
         case 4: return "THU";
         case 5: return "FRI";
         case 6: return "SAT";
-        case 7: return "SUN";
         }
 }
 
